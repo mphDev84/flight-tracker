c39f6904e03fbe3a53b8892ffb1db5dcca75af1d
Matt <91234966+mphDev84@users.noreply.github.com>
2022-06-14 10:44:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-let todosRequest = async () => {
-	const response = await fetch("https://jsonplaceholder.typicode.com/users");
-	const data = await response.json();
-	console.log("Data received is: ", data);
-};
-
-todosRequest();
 
 function populateData(e) {
 	e.preventDefault();
